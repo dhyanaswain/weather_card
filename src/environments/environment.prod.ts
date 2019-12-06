5ea7d4c0cb4c79de690e0b382dede35b147fc94a
Swain <dhyana.swain@accenture.com>
2019-12-06 22:21:21 +0530

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  appID: '8e4edaddb94e480ae1d3c04b8761b3e1',
+  gKey: '',
+  config: {
+    apiKey: "AIzaSyCEv_rqrAXLTatVFTvGUj3fuMoq-PR-2W4",
+    authDomain: "weather-22167.firebaseapp.com",
+    databaseURL: "https://weather-22167.firebaseio.com",
+    projectId: "weather-22167",
+    storageBucket: "weather-22167.appspot.com",
+    messagingSenderId: "37243553012",
+    appId: "1:37243553012:web:fae9f10c56f8bca98d8aa1",
+    measurementId: "G-Z2P7NFXP4M"
+  }
+};
